2d518bb1daa580b86b2b16896d93fae3fbbde59a
Akp2211 <palok2799@gmail.com>
2025-02-10 23:32:53 +0530

    Add files via upload

---
+++ Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int n = nums.size();
+            int idx = 1;
+            for(int i = 1; i<n; i++){
+                if(nums[i] != nums[i-1]){
+                    nums[idx] = nums[i];
+                    idx++;
+                }
+            }
+            return idx;
+        }
+    };
+    
+int main(){
+    return 0;
+}    
