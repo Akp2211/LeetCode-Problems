f086352257a91cf16dd9a62fcf260481d9669f2a
Akp2211 <palok2799@gmail.com>
2025-02-10 23:31:09 +0530

    Add files via upload

---
+++ Rotate_Image.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+            
+            //Transpose
+            for(int i =0; i<n; i++){
+            for (int j =0; j<=i; j++){
+               int temp = matrix[i][j];
+               matrix[i][j] = matrix[j][i];
+               matrix[j][i] = temp;
+            }
+         }
+         //Reverse
+            for(int k = 0; k <n; k++){
+                int i = 0;
+                int  j = n-1;
+                while(i<j){
+                int temp = matrix[k][i];
+                matrix[k][i] = matrix[k][j];
+                matrix[k][j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            return;
+        }
+    };
+    
+int main(){
+    return 0;
+}    
