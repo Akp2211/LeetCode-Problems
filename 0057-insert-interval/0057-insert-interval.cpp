5b926e18c45034c7fc031c18581a8f12706052b1
Akp2211 <palok2799@gmail.com>
2026-06-29 16:00:22 +0530

    Time: 1 ms (35.88%), Space: 21.6 MB (94.84%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int i=0; 
-        while(i<intervals.size()){
-            if(intervals[i][1] < newInterval[0]) i++;
-            else if(intervals[i][0] > newInterval[1]){
-                intervals.insert(intervals.begin()+i,newInterval);
-                return intervals;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval){
+        //O(n^2);
+        // int i=0; 
+        // while(i<intervals.size()){
+        //     if(intervals[i][1] < newInterval[0]) i++;
+        //     else if(intervals[i][0] > newInterval[1]){
+        //         intervals.insert(intervals.begin()+i,newInterval);
+        //         return intervals;
+        //     }
+        //     else{ //Overlap : Erase and merge
+        //         newInterval[0] = min(newInterval[0],intervals[i][0]);
+        //         newInterval[1] = max(newInterval[1],intervals[i][1]);
+        //         intervals.erase(intervals.begin()+i);
+        //     }
+        // }
+        // intervals.push_back(newInterval);
+        // return intervals;
+
+        //Optimised method - O(n)
+        int i = 0;
+        vector<vector<int>> result;
+        while(i < intervals.size()) {
+            if(intervals[i][1] < newInterval[0]) {
+                result.push_back(intervals[i]);
+            } else if (intervals[i][0] > newInterval[1]) {
+                break;
+            } else {
+                //Overlap : merge them
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
             }
-            else{ //Overlap : Erase and merge
-                newInterval[0] = min(newInterval[0],intervals[i][0]);
-                newInterval[1] = max(newInterval[1],intervals[i][1]);
-                intervals.erase(intervals.begin()+i);
-            }
+            i++;
         }
-        intervals.push_back(newInterval);
-        return intervals;
+        result.push_back(newInterval);
+        while(i < intervals.size()) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        return result;
     }
 };
